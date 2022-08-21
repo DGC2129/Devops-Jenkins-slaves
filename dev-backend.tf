1d8243006121591686456b1154e572a42d1d4854
DGC2129 <gopidimmiti989@gmail.com>
2022-08-22 00:33:04 +0530

    Development Base Code

--- dev-backend.tf
+++ dev-backend.tf
@@ -7,7 +7,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "devopsb25terraformdev"
+    bucket         = "devopsb25terraformdev1"
     key            = "multibranch-dev.tfstate"
     region         = "us-east-1"
     #dynamodb_table = "devopsb25-terraform-state-lock-dynamo"
